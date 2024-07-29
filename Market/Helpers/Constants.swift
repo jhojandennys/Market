68ccbd2adcdefef8ebb7072e044bf6bbac5b8e8e
jd <jhojan_54_56@hotmail.com>
2024-07-29 10:01:35 -0500

    helpers constants class

---
+++ Market/Helpers/Constants.swift
@@ -0,0 +1,58 @@
+//
+//  Constants.swift
+//  Market
+//
+//  Created by David Kababyan on 14/07/2019.
+//  Copyright © 2019 David Kababyan. All rights reserved.
+//
+
+import Foundation
+
+enum Constats {
+    static let publishableKey = "pk_test_51NfcrTBG4gmNY8OfUMP3PnZdXi0ldriA1DjGrpyKYK0INo8GzvtQxHvBoyoYtM4B5HlSqFIIK7z5BOZv21GMQPbw006FXIBMcv"
+    static let baseURLString = "https://infinite-basin-59804-a0e2e9d647ed.herokuapp.com" //"http://localhost:3000/"
+    static let defaultCurrency = "usd"
+    static let defaultDescription = "Purchase from Market"
+}
+
+//IDS and Keys
+public let kFILEREFERENCE = ""
+public let kALGFOLIA_APP_ID = "MJBHS0Y6OW"
+public let kALGOLIA_SEARCH_KEY = "26789fbbc8e531ce1301b205c76c05f0"
+public let kALGOLIA_ADMIN_KEY = "cbfa763709fc0e05e9d4fff194971e51"
+
+//Firebase Headers
+public let kUSER_PATH = "User"
+public let kCATEGORY_PATH = "Category"
+public let kITEMS_PATH = "Items"
+public let kBASKET_PATH = "Basket"
+
+
+
+//Category
+public let kNAME = "name"
+public let kIMAGENAME = "imageName"
+public let kOBJECTID = "objectId"
+
+//Item
+public let kCATEGORYID = "categoryId"
+public let kDESCRIPTION = "description"
+public let kPRICE = "price"
+public let kIMAGELINKS = "imageLinks"
+
+//Basket
+public let kOWNERID = "ownerId"
+public let kITEMIDS = "itemIds"
+
+//User
+public let kEMAIL = "email"
+public let kFIRSTNAME  = "firstName"
+public let kLASTNAME  = "lastName"
+public let kFULLNAME  = "fullName"
+public let kCURRENTUSER  = "currentUser"
+public let kFULLADDRESS  = "fullAddress"
+public let kONBOARD  = "onBoard"
+public let kPURCHASEDITEMIDS  = "purchasedItemIds"
+
+
+
